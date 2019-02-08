e53a671e8371c5585606d9921af9c04f68fc3659
ore21 <bgonzalez526@students.mchenry.edu>
2019-02-07 20:15:13 -0600

    Initial Commit

---
+++ tick tack toe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tick tack toe
+//
+//  Created by student on 2/7/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
